11459feda565095ba1fb2d7268b2d3312832b907
Teddy van Jerry <me@teddy-van-jerry.org>
2023-12-07 19:16:20 +0800

    Create NCO Using DDS in Costas Loop

---
+++ sdr-psk-fpga.gen/sources_1/bd/mref/NCO_cos_sin/xgui/NCO_cos_sin_v1_0.tcl
@@ -0,0 +1,40 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "I_WIDTH" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "O_WIDTH" -parent ${Page_0}
+
+
+}
+
+proc update_PARAM_VALUE.I_WIDTH { PARAM_VALUE.I_WIDTH } {
+	# Procedure called to update I_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.I_WIDTH { PARAM_VALUE.I_WIDTH } {
+	# Procedure called to validate I_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.O_WIDTH { PARAM_VALUE.O_WIDTH } {
+	# Procedure called to update O_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.O_WIDTH { PARAM_VALUE.O_WIDTH } {
+	# Procedure called to validate O_WIDTH
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.I_WIDTH { MODELPARAM_VALUE.I_WIDTH PARAM_VALUE.I_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.I_WIDTH}] ${MODELPARAM_VALUE.I_WIDTH}
+}
+
+proc update_MODELPARAM_VALUE.O_WIDTH { MODELPARAM_VALUE.O_WIDTH PARAM_VALUE.O_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.O_WIDTH}] ${MODELPARAM_VALUE.O_WIDTH}
+}
+
---
+++ verilog/NCO_cos_sin.v
@@ -0,0 +1,23 @@
+// Module: NCO_cos_sin
+// ===================
+// This module connects NCO (DDS) to provide the cos and sin signal.
+// It only connects the wire, and introduces zero latency.
+// 
+// Author: Wuqiong Zhao (me@wqzhao.org)
+// Date: 2023/12/07
+
+module NCO_cos_sin # (
+  parameter I_WIDTH = 32,
+  parameter O_WIDTH = 16
+) (
+  // input
+  input         [I_WIDTH-1:0] NCO_tdata,
+  input                       NCO_tvalid,
+  // output
+  output signed [O_WIDTH-1:0] NCO_cos,
+  output signed [O_WIDTH-1:0] NCO_sin
+);
+  // output assign
+  assign NCO_cos = NCO_tvalid ? NCO_tdata[O_WIDTH*1-1:0] : 0;
+  assign NCO_sin = NCO_tvalid ? NCO_tdata[O_WIDTH*2-1:O_WIDTH] : 0;
+endmodule
